863210008ac40a1f67841a0c56c9cbf5f260c688
proff.ust <proff.ust@gmail.com>
2017-10-02 21:26:48 +0300

    добавил вьюхи для show и index, описал методы index и show, создал development БД sqlite

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    render plain: params[:user].inspect
+    @user = User.new(users_params)
+    @user.save
+    redirect_to @user
   end
 
   def edit
@@ -16,5 +18,15 @@ class UsersController < ApplicationController
   end
 
   def index
+    @users = User.all
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+    def users_params
+      params.require(:user).permit(:name, :address, :is_router, :date_of_disconnect)
+    end
 end
