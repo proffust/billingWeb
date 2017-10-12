32ed7ae9d57448fdc998b61f6f282787142debc7
proff.ust <proff.ust@gmail.com>
2017-10-12 21:51:09 +0300

    реализовал работу с пользователями через апи

--- app/controllers/nats_controller.rb
+++ app/controllers/nats_controller.rb
@@ -1,12 +1,15 @@
 class NatsController < ApplicationController
   before_action :authenticate_auth_user!
+  M_HOST = "10.11.10.217"
+  M_USER = "admin"
+  M_PASS = "password"
   MTik::verbose=false
   def new
     @nat = Nat.new
   end
 
   def create
-    tik = MTik::Connection.new(:host => '10.11.10.217', :user => 'admin', :pass => 'password')
+    tik = MTik::Connection.new(:host => M_HOST, :user => M_USER, :pass => M_PASS)
     @nat = Nat.new(nats_params)
     @nat.state = true
     @nat.owner = current_auth_user.id
@@ -23,6 +26,7 @@ class NatsController < ApplicationController
       "=comment=#{@nat.name}_#{Nat.last['id']}") do |request, sentence|
         @trap = request.reply.find_sentence('!trap')
         if @trap.nil?
+          tik.close
           redirect_to action: "index"
         else
           @nat.destroy
@@ -32,7 +36,6 @@ class NatsController < ApplicationController
     else
       render 'new'
     end
-    tik.close
   end
 
   def edit
@@ -40,8 +43,23 @@ class NatsController < ApplicationController
   end
 
   def update
+    tik = MTik::Connection.new(:host => M_HOST, :user => M_USER, :pass => M_PASS)
     nat = Nat.find(params[:id])
+    edit_nat_id = tik.get_reply('/ip/firewall/nat/print',
+                                ".proplist=.id",
+                                "?comment=#{nat.name}_#{nat.id}")[0]['.id']
     if nat.update(nats_params)
+      tik.get_reply('/ip/firewall/nat/set',
+                    "=chain=dstnat",
+                    "=action=netmap",
+                    "=to-addresses=#{nat.int_ip}",
+                    "=to-ports=#{nat.int_port}",
+                    "=protocol=tcp",
+                    "=in-interface=ether2",
+                    "=dst-port=#{nat.ext_port}",
+                    "=comment=#{nat.name}_#{nat.id}",
+                    "=.id=#{edit_nat_id}")
+      tik.close
       redirect_to action: "index"
     else
       render 'edit'
@@ -57,21 +75,37 @@ class NatsController < ApplicationController
   end
 
   def activate
+    tik = MTik::Connection.new(:host => M_HOST, :user => M_USER, :pass => M_PASS)
     nat = Nat.find(params[:id])
+    edit_nat_id = tik.get_reply('/ip/firewall/nat/print',
+                                ".proplist=.id",
+                                "?comment=#{nat.name}_#{nat.id}")[0]['.id']
     nat.state = true
     nat.update(update_params)
+    tik.get_reply('/ip/firewall/nat/set',
+                  "=disabled=no",
+                  "=.id=#{edit_nat_id}")
+    tik.close
     redirect_to action: "index"
   end
 
   def deactivate
+    tik = MTik::Connection.new(:host => M_HOST, :user => M_USER, :pass => M_PASS)
     nat = Nat.find(params[:id])
+    edit_nat_id = tik.get_reply('/ip/firewall/nat/print',
+                                ".proplist=.id",
+                                "?comment=#{nat.name}_#{nat.id}")[0]['.id']
     nat.state = false
     nat.update(update_params)
+    tik.get_reply('/ip/firewall/nat/set',
+                  "=disabled=yes",
+                  "=.id=#{edit_nat_id}")
+    tik.close
     redirect_to action: "index"
   end
 
   def destroy
-    tik = MTik::Connection.new(:host => '10.11.10.217', :user => 'admin', :pass => 'password')
+    tik = MTik::Connection.new(:host => M_HOST, :user => M_USER, :pass => M_PASS)
     nat = Nat.find(params[:id])
     tik.get_reply('/ip/firewall/nat/remove',
     "=.id=#{tik.get_reply('/ip/firewall/nat/print',
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,43 @@
 class UsersController < ApplicationController
   before_action :authenticate_auth_user!
+  M_HOST = "10.11.10.217"
+  M_USER = "admin"
+  M_PASS = "password"
+  MTik::verbose=false
   def new
     @user = User.new
   end
 
   def create
+    tik = MTik::Connection.new(:host => M_HOST, :user => M_USER, :pass => M_PASS)
     @user = User.new(users_params)
     @user.access_state = true
     if @user.save
-      redirect_to action: "index"
+      tik.get_reply(
+      '/ip/firewall/address-list/add',
+      "=address=#{@user.address}",
+      "=list=#{@user.is_router ? "User_Router" : "users_new" }",
+      "=comment=#{@user.name}") do |request, sentence|
+        @trap = request.reply.find_sentence('!trap')
+        if @trap.nil?
+          tik.get_reply(
+          '/queue/simple/add',
+          "=max-limit=#{@user.is_router ? "40M/40M" : "20M/20M"}",
+          "=name=#{@user.name }",
+          "=target=#{@user.address}",
+          "=queue=default/default",
+          "=total-queue=default") do |request, sentence|
+            @trap = request.reply.find_sentence('!trap')
+            if @trap.nil?
+              tik.close
+              redirect_to action: "index"
+            else
+              @user.destroy
+              render 'new'
+            end
+          end
+        end
+      end
     else
       render 'new'
     end
@@ -20,8 +49,36 @@ class UsersController < ApplicationController
 
   def update
     user = User.find(params[:id])
+    tik = MTik::Connection.new(:host => M_HOST, :user => M_USER, :pass => M_PASS)
     if user.update(users_params)
-      redirect_to action: "index"
+      tik.get_reply('/ip/firewall/address-list/set',
+                    "=address=#{user.address}",
+                    "=list=#{user.is_router ? "User_Router" : "users_new" }",
+                    "=comment=#{user.name}",
+                    "=.id=#{tik.get_reply('/ip/firewall/address-list/print',
+                                               ".proplist=.id",
+                                               "?address=#{user.address}")[0]['.id']}") do |request, sentence|
+        @trap = request.reply.find_sentence('!trap')
+        if @trap.nil?
+          tik.get_reply('/queue/simple/set',
+                        "=target=#{user.address}",
+                        "=name=#{user.name}",
+                        "=max-limit=#{user.is_router ? "40M/40M" : "20M/20M"}",
+                        "=.id=#{tik.get_reply('/queue/simple/print',
+                                                   ".proplist=.id",
+                                                   "?target=#{user.address}/32")[0]['.id']}") do |request, sentence|
+            @trap = request.reply.find_sentence('!trap')
+            if @trap.nil?
+              tik.close
+              redirect_to action: "index"
+            else
+              render 'edit'
+            end
+          end
+        else
+          render 'edit'
+        end
+      end
     else
       render 'edit'
     end
@@ -36,29 +93,63 @@ class UsersController < ApplicationController
   end
 
   def extend
+    tik = MTik::Connection.new(:host => M_HOST, :user => M_USER, :pass => M_PASS)
     user = User.find(params[:id])
     user.date_of_disconnect = user.date_of_disconnect + 1.month
+    if Date.today < user.date_of_disconnect
+      user.access_state = true
+    end
     user.update(update_params)
+    tik.get_reply('/ip/firewall/address-list/set',
+                  "=disabled=#{user.access_state ? "no" : "yes" }",
+                  "=.id=#{tik.get_reply('/ip/firewall/address-list/print',
+                                             ".proplist=.id",
+                                             "?address=#{user.address}")[0]['.id']}")
+    tik.close
     redirect_to action: "index"
   end
 
   def deactivate
+    tik = MTik::Connection.new(:host => M_HOST, :user => M_USER, :pass => M_PASS)
     user = User.find(params[:id])
     user.access_state = false
     user.update(update_params)
+    tik.get_reply('/ip/firewall/address-list/set',
+                  "=disabled=yes",
+                  "=.id=#{tik.get_reply('/ip/firewall/address-list/print',
+                                             ".proplist=.id",
+                                             "?address=#{user.address}")[0]['.id']}")
+    tik.close
     redirect_to action: "index"
   end
 
   def activate
+    tik = MTik::Connection.new(:host => M_HOST, :user => M_USER, :pass => M_PASS)
     user = User.find(params[:id])
     user.access_state = true
-    user.update!(update_params)
+    user.update(update_params)
+    tik.get_reply('/ip/firewall/address-list/set',
+                  "=disabled=no",
+                  "=.id=#{tik.get_reply('/ip/firewall/address-list/print',
+                                             ".proplist=.id",
+                                             "?address=#{user.address}")[0]['.id']}")
+    tik.close
     redirect_to action: "index"
   end
 
   def destroy
+    tik = MTik::Connection.new(:host => M_HOST, :user => M_USER, :pass => M_PASS)
     user = User.find(params[:id])
+    tik.get_reply('/ip/firewall/address-list/remove',
+                  "=.id=#{tik.get_reply('/ip/firewall/address-list/print',
+                                             ".proplist=.id",
+                                             "?address=#{user.address}")[0]['.id']}")
+    tik.get_reply('/queue/simple/remove',
+                  "=.id=#{tik.get_reply('/queue/simple/print',
+                                             ".proplist=.id",
+                                             "?target=#{user.address}/32")[0]['.id']}")
     user.destroy
+    tik.close
     redirect_to action: "index"
   end
 
