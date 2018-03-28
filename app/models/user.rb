e740287c9cd3ab44c1854d1e3cca62223c5dccea
proff.ust <proff.ust@gmail.com>
2018-03-28 17:23:25 +0300

    Fix #4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :address, format: { with: /\A^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\z/,
     message: "Must IP address" }
+  validates :date_of_disconnect, :name, presence: true
   validates :address, :name, uniqueness: true
-  validates :date_of_disconnect, presence: true
 end
