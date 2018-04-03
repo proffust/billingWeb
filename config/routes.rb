6fba841175870f65e8ab2bc21dfe5b1fc635b1cc
proff.ust <proff.ust@gmail.com>
2018-04-03 16:21:48 +0300

    Fix #7

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users do
     member do
-      get 'activate'
-      get 'deactivate'
-      get 'extend'
+      patch 'activate'
+      patch 'deactivate'
+      patch 'extend'
     end
   end
   resources :nats do
     member do
-      get 'activate'
-      get 'deactivate'
+      patch 'activate'
+      patch 'deactivate'
     end
   end
   root 'persons#profile'
